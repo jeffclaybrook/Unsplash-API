a16e15e988134e30810640485da799c68b339bda
jeffclaybrook <48254646+jeffclaybrook@users.noreply.github.com>
2023-04-03 13:10:33 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ function toggleImageLike() {
     const icon = button.querySelector('.material-symbols-rounded');
     button.classList.toggle('filled');
     button.classList.contains('filled') ?
-    icon.style.fontVariationSettings = `'FILL' 1` :
-    icon.style.fontVariationSettings = `'FILL' 0`
+    icon.style.fontVariationSettings = `'FILL' 1, 'wght' 300` :
+    icon.style.fontVariationSettings = `'FILL' 0, 'wght' 300`
 }
 
 
@@ -35,23 +35,23 @@ function downloadImage(image) {
 }
 
 
-function showToast(color) {
+function showToast(hex) {
     const toast = document.querySelector('.toast');
-    toast.classList.add('visible');
-    toast.setAttribute('aria-hidden', false);
-    toast.innerText = `Copied ${color}`;
+    toast.classList.add('active');
+    toast.setAttribute('aria-hidden', 'false');
+    toast.innerText = `Copied HEX code: ${hex}`;
     setTimeout(() => {
-        toast.classList.remove('visible');
-        toast.setAttribute('aria-hidden', true);
+        toast.classList.remove('active');
+        toast.setAttribute('aria-hidden', 'true');
         toast.innerText = '';
     }, 2000);
 }
 
 
-function copyColor(color) {
-    const clipboard = navigator.clipboard;
-    clipboard.writeText(color)
-    .then(() => showToast(color));
+function copyPalette(palette) {
+    const color = palette;
+    navigator.clipboard.writeText(color)
+    .then(() => showToast(color))
 }
 
 
@@ -59,9 +59,9 @@ function closeDialog() {
     const body = document.body;
     const dialog = document.querySelector('dialog');
     body.style.overflow = 'auto';
-    dialog.setAttribute('aria-hidden', true);
+    dialog.setAttribute('aria-hidden', 'true');
     dialog.classList.remove('expanded');
-    dialog.innerHTML = '';
+    dialog.querySelector('.dialog-container').innerHTML = '';
 }
 
 
@@ -73,6 +73,7 @@ function openDialog(
     likes,
     width,
     regular,
+    raw,
     first_name,
     last_name,
     location,
@@ -84,50 +85,56 @@ function openDialog(
     const body = document.body;
     const dialog = document.querySelector('dialog');
     body.style.overflow = 'hidden';
-    dialog.setAttribute('aria-hidden', false);
+    dialog.setAttribute('aria-hidden', 'false');
     dialog.classList.add('expanded');
-    dialog.innerHTML = `
-    <div class="dialog-scrim"></div>
-    <div class="dialog-container">
-        <div class="dialog-header">
-            <a href="${html}" target="_blank">
-                <img src="${medium}" alt="${name}">
-                <div class="column">
-                    <h3>${first_name} ${last_name}</h3>
-                    <p>${location}</p>
-                </div>
-            </a>
-            <button aria-label="Close image" onclick="closeDialog()">
-                <i class="material-symbols-rounded">close</i>
-            </button>
-        </div>
-        <div class="dialog-content">
-            <img alt="${alt_description}" style="aspect-ratio: ${width} / ${height}" src="${regular}">
-        </div>
-        <div class="dialog-footer">
-            <div class="row">
-                <button class="like-btn" onclick="toggleImageLike()">
+    dialog.querySelector('.dialog-container').innerHTML += `
+    <div class="dialog-header">
+        <a href="${html}" target="_blank" aria-label="View portfolio">
+            <img src="${medium}" alt="${name}">
+            <div class="column">
+                <h3>${first_name} ${last_name}</h3>
+                <p>${location}</p>
+            </div>
+        </a>
+        <button aria-label="Close image" onclick="closeDialog()">
+            <i class="material-symbols-rounded">close</i>
+        </button>
+    </div>
+    <div class="dialog-content">
+        <img alt="${alt_description}" style="aspect-ratio: ${width} / ${height}" src="${regular}">
+    </div>
+    <div class="dialog-footer">
+        <div class="row">
+            <div class="column">
+                <button class="like-btn" onclick="toggleImageLike()" aria-label="Toggle image like">
                     <i class="material-symbols-rounded">favorite</i>
                 </button>
-                <button>
+            </div>
+            <div class="column">
+                <a href="${raw}" target="_blank" aria-label="Open image in new tab">
                     <i class="material-symbols-rounded">open_in_new</i>
-                </button>
-                <button onclick="copyColor('${color}')">
+                </a>
+            </div>
+            <div class="column">
+                <button onclick="copyPalette('${color}')" aria-label="Copy HEX code">
                     <i class="material-symbols-rounded" style="color: ${color}">launcher_assistant_on</i>
                 </button>
-                <button>
+            </div>
+            <div class="column">
+                <button aria-label="Share image">
                     <i class="material-symbols-rounded">send</i>
                 </button>
-                <button onclick="downloadImage('${regular}')">
+            </div>
+            <div class="column">
+                <button onclick="downloadImage('${regular}')" aria-label="Download image">
                     <i class="material-symbols-rounded">download</i>
                 </button>
             </div>
-            <h3>Likes ${likes}</h3>
-            <h4><strong>${username}</strong> ${alt_description}</h4>
-            <h5>${created_at}</h5>
         </div>
+        <h3>Likes ${likes}</h3>
+        <h4><strong>${username}</strong> ${alt_description}</h4>
+        <h5>${created_at}</h5>
     </div>
-    <div class="toast" aria-hidden="true"></div>
     `;
 }
 
@@ -140,9 +147,10 @@ function createGalleryItems(data) {
             created_at,
             height,
             likes,
-            width
+            width,
+            urls,
+            user
         } = item;
-        const { regular } = item.urls;
         const {
             first_name,
             last_name,
@@ -151,17 +159,19 @@ function createGalleryItems(data) {
             username,
             profile_image,
             links
-        } = item.user;
+        } = user;
+
+        const { regular, raw } = urls;
         const { medium } = profile_image;
         const { html } = links;
 
         const formattedLikes = likes.toLocaleString('en-US');
-        const formattedDate = new Date(created_at).toLocaleTimeString('en-US');
+        const formattedDate = new Date(created_at).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
         const newLocation = location == null ? 'Austin, TX' : location;
         const newLastName = last_name == null ? '' : last_name;
 
         return `
-        <li class="item" style="background-image: url('${regular}')" onclick="openDialog('${alt_description}', '${color}', '${formattedDate}', '${height}', '${formattedLikes}', '${width}', '${regular}', '${first_name}', '${newLastName}', '${newLocation}', '${name}', '${username}', '${medium}', '${html}')">
+        <li class="item" style="background-image: url('${regular}')" onclick="openDialog('${alt_description}', '${color}', '${formattedDate}', '${height}', '${formattedLikes}', '${width}', '${regular}', '${raw}', '${first_name}', '${newLastName}', '${newLocation}', '${name}', '${username}', '${medium}', '${html}')">
             <div class="item-footer">
                 <div class="flex">
                     <img src="${medium}" alt="${name}">
@@ -208,7 +218,7 @@ getData(`https://api.unsplash.com/search/photos/?client_id=${accessKey}&query=${
 
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY + window.innerHeight >= document.body.offsetHeight - 15) {
+    if (window.scrollY + window.innerHeight >= document.body.offsetHeight - 30) {
         showLoader();
     }
 })
